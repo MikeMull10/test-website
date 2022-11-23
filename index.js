dd86f937511838e36ebf16e61e96bfc6a598f4a7
MikeMull10 <50462800+MikeMull10@users.noreply.github.com>
2022-11-23 14:33:46 -0500

    CORS

--- index.js
+++ index.js
@@ -13,4 +13,12 @@ app.get('/auth/discord', (request, response) => {
 	return response.sendFile('dashboard.html', { root: '.' });
 });
 
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+});
+
 app.listen(process.env.PORT || 3000);
